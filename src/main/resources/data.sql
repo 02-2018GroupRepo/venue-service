23a5b45b40e0108c7ec12a88b94546a0fa529678
syntel <huntmac10@gmail.com>
2018-04-04 14:07:09 -0400

    Added h2 test database, schema and data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO venue_practice
+(id, venuename, address, parking, eventid, capacity)
+VALUES(1, 'Terminal West', '123 West Elm Street', true, 1, 800);
+
+INSERT INTO venue_practice
+(id, venuename, address, parking, eventid, capacity)
+VALUES(2, 'The Masquerade', '123 Peachtree Street', true, 2, 1200);
+
+INSERT INTO venue_practice
+(id, venuename, address, parking, eventid, capacity)
+VALUES(3, 'Mercedes Benz', '123 Mercedes Benz Drive', true, 3, 800);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE public.venue_practice (
+	id int4 NULL,
+	venuename varchar(255) NULL,
+	address varchar(255) NULL,
+	parking bool NULL,
+	eventid int4 NULL,
+	capacity int4 NULL
+);
