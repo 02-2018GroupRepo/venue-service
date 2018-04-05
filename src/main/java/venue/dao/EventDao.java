7fb97968af8d16c7da76e574561dc6e6998a21bf
syntel <huntmac10@gmail.com>
2018-04-05 17:03:22 -0400

    Added environment variable to eventDao

--- src/main/java/venue/dao/EventDao.java
+++ src/main/java/venue/dao/EventDao.java
@@ -1,6 +1,7 @@
 package venue.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -9,11 +10,14 @@ import venue.model.Event;
 
 @Component
 public class EventDao {
+	@Value("${local.path}")
+	private String localPath;
+	
 	@Autowired
 	RestTemplate restTemplate;
 	
 	public Event getEventById(int id) {
-		String transactionUrl = "http://localhost:8080/event/" + id;
+		String transactionUrl = localPath + id;
         UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(transactionUrl);
         Event response = (Event) restTemplate.getForObject(builder.toUriString(), Event.class);
         return response;
